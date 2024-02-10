073ea7679cf36f6d54f1dde2cdc538eed774688d
Cristina Ortega <cristinamaria.ortega671@comunidadunir.net>
2024-02-10 12:43:48 +0100

    Update todoApiTest.py

--- test/integration/todoApiTest.py
+++ test/integration/todoApiTest.py
@@ -7,8 +7,8 @@ import json
 import pytest
 
 
-BASE_URL = os.environ.get(BASE_URI)
-#BASE_URL = BASE_URI
+#BASE_URL = os.environ.get(BASE_URI)
+BASE_URL = BASE_URI
 DEFAULT_TIMEOUT = 2  # in secs
 
 
